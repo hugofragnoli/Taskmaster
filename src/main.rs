e4959718cfe2f5bc1dfac9bcef48647a8a68d572
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-25 16:30:50 +0100

    ajout test serde main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+struct ProgramConfig {
+    cmd: String,
+    numprocs: u32,
+    autostart: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct TaskmasterConfig {
+    programs: HashMap<String, ProgramConfig>,
+}
+
 fn main() {
-    let hugo = "Hello World";
-    println!("{hugo}");
+    let f = std::fs::File::open("config.yaml").expect("Fichier introuvable");
+    let config: TaskmasterConfig = serde_yaml::from_reader(f).expect("Erreur de parsing");
+
+    if let Some(p) = config.programs.get("my_ping") {
+        println!("La commande à lancer est : {}", p.cmd);
+    }
 }
